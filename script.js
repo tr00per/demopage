f1c6b1e25b7963b75c28ea98450a849761bb750b
Artur Czajka <arturczajka@gmail.com>
2016-10-19 22:59:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+//(function () {
+//    function init() {
+//        var tables = document.getElementsByTagName("table");
+//        tables[0].innerHTML += "<caption>Podpis do tabelki</caption>";
+//
+//        var coverup = document.getElementById("curtain");
+//        coverup.addEventListener("click", closeCurtain(coverup), true);
+//    }
+//
+//    function closeCurtain(curtain) {
+//        return function (ev) {
+//            curtain.style.display = "none";
+//        }
+//    }
+//
+//    window.onload = init
+//})();
+
 (function () {
     function init() {
-        var tables = document.getElementsByTagName("table");
-        tables[0].innerHTML += "<caption>Podpis do tabelki</caption>";
+        let tables = document.querySelectorAll("table");
+        tables.forEach(elem => elem.innerHTML += "<caption>Podpis do tabelki</caption>");
 
-        var coverup = document.getElementById("curtain");
+        let coverup = document.querySelector("#curtain");
         coverup.addEventListener("click", closeCurtain(coverup), true);
     }
 
